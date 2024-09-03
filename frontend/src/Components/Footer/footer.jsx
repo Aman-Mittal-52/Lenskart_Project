161f74034e9c5f08053eb45d101cb39cef3afc4f
Aman <amanmittle1234@gmail.com>
2024-09-04 01:43:29 +0530

    footer responive (completed)

--- frontend/src/Components/Footer/footer.jsx
+++ frontend/src/Components/Footer/footer.jsx
@@ -1,15 +1,64 @@
 import React from 'react'
 import {
     Box,
-    Flex
+    Divider,
+    Flex,
+    Image,
+    Text,
 } from '@chakra-ui/react'
+
+
+import FooterSection from './footer_section'
+import FooterAppSection from './footer_app_section'
+import FooterSocialSection from './footer_social_section'
 
 function Footer() {
     return (
-        <Box className='footer' h={'500px'} bg='#000042' px={16} py={8}>
-            <Flex>
-                
+        <Box
+            className='footer'
+            bg='#000042'
+            color='#FFF'
+            px={[6, 8, 10, 16]}
+            py={[2, 3, 6, 8]}>
+
+            <Flex
+                justify='space-between'
+                flexDirection={{ base: 'column', md: 'row' }}>
+
+                <Flex justify='space-between'>
+
+                    <FooterSection
+                        heading='Services'
+                        textArray={[
+                            'Store Locator',
+                            'Buying Guide',
+                            'Frame Size'
+                        ]} />
+
+                    <FooterSection
+                        heading='About Us'
+                        textArray={[
+                            'We Are Hiring',
+                            'Refer And Earn',
+                            'About Us',
+                            'Lenskart Coupons'
+                        ]} />
+
+                    <FooterSection
+                        heading='Help'
+                        textArray={[
+                            "FAQ's"
+                        ]} />
+
+                </Flex>
+
+                <FooterAppSection />
+
             </Flex>
+
+            <Divider bg='white' />
+            
+            <FooterSocialSection />
         </Box>
     )
 }
