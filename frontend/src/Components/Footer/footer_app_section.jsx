28584a96521283481d1ac9e83eadfa7ef3535e2b
Aman <amanmittle1234@gmail.com>
2024-09-04 02:03:44 +0530

    redirect to app

--- frontend/src/Components/Footer/footer_app_section.jsx
+++ frontend/src/Components/Footer/footer_app_section.jsx
@@ -1,17 +1,24 @@
 import React from 'react'
 
-import { Box, Flex, Image, Text } from '@chakra-ui/react'
+import { Box, Flex, Image, Link, Text } from '@chakra-ui/react'
 
 import play_store from '../../assets/footer/play_store.svg'
 import app_store from '../../assets/footer/app_store.svg'
+import { useNavigate } from 'react-router-dom'
 
 function FooterAppSection() {
+    const mobileApp = useNavigate()
+
+    const handleClick = () => {
+        mobileApp('/mobile-app')
+    }
+
     return (
         <Box p={[2, 3, 4, 5]}>
 
             <Flex w={'100%'} justify='center' flexDirection={{ base: 'column', md: 'column', lg: 'row' }} align='center'>
-                <Image src={play_store} w='40%' p='2' />
-                <Image src={app_store} w='40%' p='2' />
+                <Image src={play_store} onClick={handleClick} w='40%' p='2' />
+                <Image src={app_store} onClick={handleClick} w='40%' p='2' />
             </Flex>
 
             <Box textAlign='center'>
