3414ce2cc6841008e69124b349f1ede4651d7c0b
Aman <amanmittle1234@gmail.com>
2024-09-04 01:49:46 +0530

    link section

---
+++ frontend/src/Components/Footer/footer_social_section.jsx
@@ -0,0 +1,42 @@
+import React from 'react'
+
+import { Box, Flex, Link, Text } from '@chakra-ui/react'
+import { FaFacebookSquare, FaInstagram, FaTwitter } from 'react-icons/fa'
+
+function FooterSocialSection() {
+    return (
+        <Flex
+            mt={4}
+            justify='space-between'
+            align='center'
+            flexDirection={{ base: 'column', md: 'column', lg: 'row' }}>
+
+            <Flex justify='space-between' w={{ base: '100%', md: '100%', lg: '20%' }}>
+                <Text>T&C</Text>
+                <Text>Privacy</Text>
+                <Text>Disclaimer</Text>
+            </Flex>
+
+            <Flex
+                justify='space-between'
+                align={'center'}
+                w={{ base: '100%', md: '100%', lg: '25%' }}
+                mt='4'
+            >
+                <Text>Version 1.0.0 || Flollow Us</Text>
+                <Link href='https://www.facebook.com/Lenskartindia'>
+                <FaFacebookSquare fontSize={['10px', '15px', '20px', '30px']} />
+                </Link>
+                <Link href='https://www.instagram.com/lenskart/'>
+                <FaInstagram fontSize={['10px', '15px', '20px', '30px']} />
+                </Link>
+                <Link href='https://x.com/lenskartME'>
+                <FaTwitter fontSize={['10px', '15px', '20px', '30px']} />
+                </Link>
+            </Flex>
+
+        </Flex>
+    )
+}
+
+export default FooterSocialSection
