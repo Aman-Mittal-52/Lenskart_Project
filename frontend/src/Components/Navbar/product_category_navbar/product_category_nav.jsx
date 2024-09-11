bf97ad7f17851c51c172ff8f06aabd5a3d6a284e
Aman <amanmittle1234@gmail.com>
2024-09-12 03:48:02 +0530

    Navbar drop down

--- frontend/src/Components/Navbar/product_category_navbar/product_category_nav.jsx
+++ frontend/src/Components/Navbar/product_category_navbar/product_category_nav.jsx
@@ -1,8 +1,8 @@
 import { Box, Flex, Image, Show } from '@chakra-ui/react'
 import React from 'react'
-import tryon3d from "../../../assets/tryon3d.webp"
-import blulogo from "../../../assets/blulogo.webp"
-import gold_max_logo from "../../../assets/gold_max_logo.webp"
+import tryon3d from "../../../assets/navbar/tryon3d.webp"
+import blulogo from "../../../assets/navbar/blulogo.webp"
+import gold_max_logo from "../../../assets/navbar/gold_max_logo.webp"
 import CategoryCollapse from './category_collapse'
 import Imagebox from './image_box'
 
@@ -16,18 +16,18 @@ function ProductCategoryNav() {
                 justify={'space-between'}
                 align={'center'}>
                 <Flex justify={'space-around'}>
-                    <CategoryCollapse text='eyeglasses' />
-                    <CategoryCollapse text='screen glasses' />
-                    <CategoryCollapse text='kids glasses' />
-                    <CategoryCollapse text='contact lenses' />
-                    <CategoryCollapse text='sunglasses' />
-                    <CategoryCollapse text='home eye-test' />
-                    <CategoryCollapse text='store locator' />
+                    <CategoryCollapse text='eyeglasses' component='glasses' />
+                    <CategoryCollapse text='screen glasses' component='glasses' />
+                    <CategoryCollapse text='kids glasses' component='kids' />
+                    <CategoryCollapse text='contact lenses' component='lenses' />
+                    <CategoryCollapse text='sunglasses' component='glasses' />
+                    <CategoryCollapse text='home eye-test' component='home' />
+                    <CategoryCollapse text='store locator' component='store' />
                 </Flex>
                 <Flex>
-                <Imagebox image={tryon3d} alt='3d_trial' />
-                <Imagebox image={blulogo} alt='blu_logo' />
-                <Imagebox image={gold_max_logo} alt='gold_max_logo' />
+                    <Imagebox image={tryon3d} alt='3d_trial' />
+                    <Imagebox image={blulogo} alt='blu_logo' />
+                    <Imagebox image={gold_max_logo} alt='gold_max_logo' />
                 </Flex>
             </Flex>
         </Show>
